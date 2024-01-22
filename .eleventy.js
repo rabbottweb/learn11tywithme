2c9a3f28b1180873f832d8dea458a5b5fe6dfa27
ryana <ryan@ryanabbott.net>
2024-01-22 17:23:39 -0500

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(pluginWebc);
+
+    eleventyConfig.addPassthroughCopy("dev/assets");
+
+    return {
+        dir: {
+            input: "dev",
+            output: "public",
+            layouts: "_includes/layouts"
+        },
+        templateFormats: ["webc", "md", "njk", "html", "liquid", "css" ]
+    };
+};
